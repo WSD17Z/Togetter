56ed3b5e75402b2f9b403fb750733722f3728494
Michał Dzięciołowski <michaldzie@gmail.com>
2017-12-17 13:19:09 +0100

    Add maps modules to calculate rute distanse duration

---
+++ app/src/main/java/wsd17z/togetter/MapsModules/Duration.java
@@ -0,0 +1,11 @@
+package wsd17z.togetter.MapsModules;
+
+public class Duration {
+    public String text;
+    public int value;
+
+    public Duration(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+}
